0331d74bd6deceeab2b3761e6de5e2f312cd02cd
hallwayskiing <541814852@qq.com>
2023-03-16 15:42:25 +0800

    type_completed

--- MiniGames/TypeForm.Designer.cs
+++ MiniGames/TypeForm.Designer.cs
@@ -28,19 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            inputBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            showLabel = new Label();
+            label4 = new Label();
+            startBtn = new Button();
+            label3 = new Label();
+            timeLabel = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            subBtn = new Button();
+            endLabel = new Label();
             SuspendLayout();
+            // 
+            // inputBox
+            // 
+            inputBox.Enabled = false;
+            inputBox.Location = new Point(160, 151);
+            inputBox.Name = "inputBox";
+            inputBox.Size = new Size(251, 23);
+            inputBox.TabIndex = 0;
+            inputBox.KeyPress += inputBox_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(192, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Type";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(73, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Sentence:";
+            // 
+            // showLabel
+            // 
+            showLabel.AutoEllipsis = true;
+            showLabel.AutoSize = true;
+            showLabel.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            showLabel.Location = new Point(157, 58);
+            showLabel.Name = "showLabel";
+            showLabel.Size = new Size(186, 21);
+            showLabel.TabIndex = 3;
+            showLabel.Text = "Type the same as here!";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(107, 150);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 21);
+            label4.TabIndex = 4;
+            label4.Text = "Type:";
+            // 
+            // startBtn
+            // 
+            startBtn.Location = new Point(180, 203);
+            startBtn.Name = "startBtn";
+            startBtn.Size = new Size(75, 23);
+            startBtn.TabIndex = 5;
+            startBtn.Text = "Start";
+            startBtn.UseVisualStyleBackColor = true;
+            startBtn.Click += startBtn_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(100, 106);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 21);
+            label3.TabIndex = 6;
+            label3.Text = "Timer:";
+            // 
+            // timeLabel
+            // 
+            timeLabel.AutoSize = true;
+            timeLabel.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            timeLabel.Location = new Point(157, 106);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(155, 21);
+            timeLabel.TabIndex = 7;
+            timeLabel.Text = "Value of timeDelay";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // subBtn
+            // 
+            subBtn.Location = new Point(180, 203);
+            subBtn.Name = "subBtn";
+            subBtn.Size = new Size(75, 23);
+            subBtn.TabIndex = 8;
+            subBtn.Text = "Submit";
+            subBtn.UseVisualStyleBackColor = true;
+            subBtn.Visible = false;
+            subBtn.Click += subBtn_Click;
+            // 
+            // endLabel
+            // 
+            endLabel.AutoSize = true;
+            endLabel.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            endLabel.Location = new Point(143, 205);
+            endLabel.Name = "endLabel";
+            endLabel.Size = new Size(0, 21);
+            endLabel.TabIndex = 9;
             // 
             // TypeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(448, 247);
+            Controls.Add(endLabel);
+            Controls.Add(subBtn);
+            Controls.Add(timeLabel);
+            Controls.Add(label3);
+            Controls.Add(startBtn);
+            Controls.Add(label4);
+            Controls.Add(showLabel);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(inputBox);
             Name = "TypeForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Type";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox inputBox;
+        private Label label1;
+        private Label label2;
+        private Label showLabel;
+        private Label label4;
+        private Button startBtn;
+        private Label label3;
+        private Label timeLabel;
+        private System.Windows.Forms.Timer timer1;
+        private Button subBtn;
+        private Label endLabel;
     }
 }
--- MiniGames/TypeForm.cs
+++ MiniGames/TypeForm.cs
@@ -12,9 +12,81 @@ namespace MiniGames
 {
     public partial class TypeForm : Form
     {
+        string[] typelist;
+        int timeCount;
+        int timeDelay;
+        int listLength;
+        int correctNum;
+        int inputTimes;
+        int randomNum;
+        Random random;
+
         public TypeForm()
         {
             InitializeComponent();
+            typelist = File.ReadAllLines("D:\\code\\MiniGames\\MiniGames\\typelist.txt");
+            listLength = typelist.Length;
+            timeDelay = 10;
+            timeCount = timeDelay;
+            correctNum = 0;
+            inputTimes = 0;
+            randomNum = 0;
+            random = new Random();
+            timeLabel.Text = timeDelay.ToString();
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            startBtn.Visible = false;
+            subBtn.Visible = true;
+            inputBox.Enabled = true;
+            inputBox.Focus();
+            randomNum = random.Next(listLength);
+            showLabel.Text = typelist[randomNum];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLabel.Text = (--timeCount).ToString();
+            if (timeCount < 0)
+                subBtn_Click(sender, e);
+        }
+
+        private void subBtn_Click(object sender, EventArgs e)
+        {
+            timeLabel.Text = (timeCount = timeDelay).ToString();
+            string input = inputBox.Text;
+            if (input == showLabel.Text)
+                correctNum++;
+            inputBox.Clear();
+
+            int tmp;
+            do
+            {
+                tmp = random.Next(listLength);
+            } while (tmp == randomNum);
+            randomNum = tmp;
+            showLabel.Text = typelist[randomNum];
+
+            if (++inputTimes == 10)
+            {
+                subBtn.Visible = false;
+                timer1.Enabled = false;
+                inputBox.Enabled = false;
+                endLabel.Text = "Game over! Correct: " + correctNum.ToString();
+
+            }
+        }
+
+        private void inputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == System.Convert.ToChar(13))
+            {
+                e.Handled = true;
+                subBtn_Click(sender, e);
+            }
         }
     }
 }
